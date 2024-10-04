3e66a535426319be22580c7fb93fb61deb06a0d3
erc83 <espinoza.eric@gmail.com>
2024-10-04 11:40:32 -0300

    add: table page

---
+++ pages/tables.tsx
@@ -0,0 +1,38 @@
+import { mdiGithub, mdiMonitorCellphone, mdiTableBorder, mdiTableOff } from "@mdi/js";
+import React, { ReactElement } from "react";
+import SectionMain from "../components/SectionMain";
+import SectionTitleLineWithButton from "../components/SectionTitleLineWithButton";
+import BaseButton from "../components/BaseButton";
+import LayoutAuthenticated from "../components/layouts/Authenticated";
+import NotificationBar from "../components/NotificationBar";
+
+const TablesPage = () => {
+    return (
+        <SectionMain>
+            <SectionTitleLineWithButton icon={mdiTableBorder} title="Tables" main>
+                <BaseButton
+                    href="https://github.com/justboil/admin-one-vue-tailwind"
+                    target="_blank"
+                    icon={mdiGithub}
+                    label="Star on Github"
+                    color="contrast"
+                    roundedFull
+                    small
+                />
+            </SectionTitleLineWithButton>
+
+            <NotificationBar color="info" icon={mdiMonitorCellphone}>
+                <b>Responsive table.</b> Collapses on mobile
+            </NotificationBar>
+
+        </SectionMain>
+    )
+}
+
+TablesPage.getLayout = function getLayout(page: ReactElement) {
+    return <LayoutAuthenticated>{page}</LayoutAuthenticated>
+}
+
+export default TablesPage;
+
+
