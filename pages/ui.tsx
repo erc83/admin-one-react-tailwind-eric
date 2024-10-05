b56b1440b69cf1518d1510841beee6e8b8aeb7c6
erc83 <espinoza.eric@gmail.com>
2024-10-05 19:49:39 -0300

    add: IU page: NotificationBar

--- pages/ui.tsx
+++ pages/ui.tsx
@@ -1,18 +1,70 @@
+import {
+    mdiAlert,
+    mdiAlertCircle,
+    mdiCheckCircle,
+    mdiContrastCircle,
+    mdiInformation,
+    mdiOpenInNew,
+    mdiReload,
+    mdiTrendingUp,
+} from '@mdi/js'
 import Head from 'next/head'
 import { ReactElement } from 'react'
 import LayoutAuthenticated from '../components/layouts/Authenticated'
 import SectionTitle from '../components/SectionTitle'
 import { appTitle } from '../src/config'
+import { useAppDispatch } from '../src/stores/hooks'
+import BaseButton from '../components/BaseButton'
+import BaseButtons from '../components/BaseButtons'
+import SectionMain from '../components/SectionMain'
+import NotificationBar from '../components/NotificationBar'
 
 const UiPage = () => {
+    
+    const dispatch = useAppDispatch()
+
+    const NotificationSampleButton = <BaseButton color="white" label="Button" roundedFull small />
+
+    const CardSamplesFooter = (
+        <BaseButtons>
+            <BaseButton label="Confirm" color="info" isGrouped />
+            <BaseButton label="Cancel" color="info" isGrouped outline/>
+        </BaseButtons>
+    )
+
+
     return (
         <>
             <Head>
                 <title>UI - {appTitle}</title>
             </Head>
-            <div className="min-h-screen">
-                <SectionTitle first>Demo is coming soon</SectionTitle>
-            </div>
+
+            <SectionTitle first>Dark mode</SectionTitle>
+        
+            <SectionMain>
+                <NotificationBar color="info" icon={mdiInformation} button={NotificationSampleButton}>
+                    <b>Info State</b>. NotificationBar
+                </NotificationBar>
+
+                <NotificationBar color="success" icon={mdiCheckCircle} button={NotificationSampleButton}>
+                    <b>Success State</b>. NotificationBar
+                </NotificationBar>
+
+                <NotificationBar color="warning" icon={mdiAlert} button={NotificationSampleButton} >
+                    <b>Warning State</b>. NotificationBar
+                </NotificationBar>
+                
+                <NotificationBar color="danger" icon={mdiAlertCircle} button={NotificationSampleButton} >
+                    <b>Danger State</b>. NotificationBar
+                </NotificationBar>
+                
+                <NotificationBar color="contrast" icon={mdiContrastCircle} >
+                    <b>Contrast</b>. NotificationBar
+                </NotificationBar>
+
+
+
+            </SectionMain>
         </>
     )
 }
