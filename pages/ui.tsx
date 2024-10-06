1ae79a978a98004b0861d03fa460f612d0a989f5
erc83 <espinoza.eric@gmail.com>
2024-10-05 21:05:13 -0300

    add: Ui Page: BaseButton

--- pages/ui.tsx
+++ pages/ui.tsx
@@ -20,6 +20,7 @@ import SectionMain from '../components/SectionMain'
 import NotificationBar from '../components/NotificationBar'
 import CardBox from '../components/CardBox'
 import { setDarkMode } from '../src/stores/styleSlice'
+import BaseDivider from '../components/BaseDivider'
 
 const UiPage = () => {
     
@@ -55,9 +56,7 @@ const UiPage = () => {
                 </CardBox>
             </SectionMain>
 
-
-
-        
+            <SectionTitle first>Notifications</SectionTitle>
             <SectionMain>
                 <NotificationBar color="info" icon={mdiInformation} button={NotificationSampleButton}>
                     <b>Info State</b>. NotificationBar
@@ -78,6 +77,44 @@ const UiPage = () => {
                 <NotificationBar color="contrast" icon={mdiContrastCircle} >
                     <b>Contrast</b>. NotificationBar
                 </NotificationBar>
+
+                <SectionTitle>Buttons</SectionTitle>
+                <SectionMain>
+                    <CardBox>
+                        <BaseButtons>
+                            <BaseButton color="lightDark" label="Button" isGrouped/>
+                            <BaseButton color="contrast" label="Button" isGrouped/>
+                            <BaseButton color="info" label="Button" isGrouped/>
+                            <BaseButton color="success" label="Button" isGrouped/>
+                            <BaseButton color="warning" label="Button" isGrouped/>
+                            <BaseButton color="danger" label="Button" isGrouped/>
+                        </BaseButtons>
+
+                        <BaseDivider />
+
+                        <BaseButtons>
+                            <BaseButton color="lightDark" label="Button" icon={mdiOpenInNew} isGrouped/>
+                            <BaseButton color="contrast" label="Button" icon={mdiOpenInNew} isGrouped/>
+                            <BaseButton color="info" label="Button" icon={mdiOpenInNew} isGrouped/>
+                            <BaseButton color="success" label="Button" icon={mdiOpenInNew} isGrouped/>
+                            <BaseButton color="warning" label="Button" icon={mdiOpenInNew} isGrouped/>
+                            <BaseButton color="danger" label="Button" icon={mdiOpenInNew} isGrouped/>
+                        </BaseButtons>
+
+                        <BaseDivider />
+
+                        <BaseButtons>
+                            <BaseButton color="lightDark" icon={mdiOpenInNew} isGrouped/>
+                            <BaseButton color="contrast" icon={mdiOpenInNew} isGrouped/>
+                            <BaseButton color="info" icon={mdiOpenInNew} isGrouped/>
+                            <BaseButton color="success" icon={mdiOpenInNew} isGrouped/>
+                            <BaseButton color="warning" icon={mdiOpenInNew} isGrouped/>
+                            <BaseButton color="danger" icon={mdiOpenInNew} isGrouped/>
+                        </BaseButtons>
+
+                    </CardBox>
+                </SectionMain>
+
 
 
 
