5c41023368a1ff669dfe8ebcfa81ad1ee331ab5d
erc83 <espinoza.eric@gmail.com>
2024-10-06 20:31:09 -0300

    add className CardBoxComponentBody and Footer

--- components/CardBoxComponentBody.tsx
+++ components/CardBoxComponentBody.tsx
@@ -1,10 +1,12 @@
 import React, { ReactNode } from "react"
 
 type Props = {
-    noPadding?: boolean,
+    noPadding?: boolean
+    className: string
     children?: ReactNode
 }
 
-export default function CardBoxComponentBody({ noPadding=false, children }: Props) {
-    return <div className={`flex-1 ${noPadding ? '' : 'p-6'}`}>{children}</div>
+export default function CardBoxComponentBody({ noPadding=false, className, children }: Props) {
+    return <div className={`flex-1 ${noPadding ? '' : 'p-6'} ${className} `}>{children}</div>
 }
+
