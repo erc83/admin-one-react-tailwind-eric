8644d4870a3653282ab8c78f050a7d3835ba7bb8
erc83 <espinoza.eric@gmail.com>
2024-10-06 02:42:01 -0300

    upd: forms: dropdown, textarea

--- pages/forms.tsx
+++ pages/forms.tsx
@@ -11,6 +11,7 @@ import CardBox from '../components/CardBox'
 import { Formik, Form, Field } from 'formik'
 import FormField from '../components/FormField'
 import BaseButtons from '../components/BaseButtons'
+import BaseDivider from '../components/BaseDivider'
 
 const FormsPage = () => {
     return (
@@ -38,18 +39,41 @@ const FormsPage = () => {
                             fullname: 'John Doe',
                             email: 'john.doe@example.com',
                             phone: '',
-                            color: 'green',
+                            dropdown: 'green',
                             textarea: 'Hello',
                         }}
                         onSubmit={(values) => alert(JSON.stringify(values, null, 2))}
                     >
                         <Form> 
-                            <FormField label="Grouped whit icons" icons={[mdiAccount, mdiMail]}>
+                            <FormField label="Grouped whit icons Name and Email" icons={[mdiAccount, mdiMail]}>
                                 <Field name="fullname" placeholder="Full name" />
                                 <Field type="email" name="email" placeholder="Email" />
                             </FormField>
 
+                            <FormField
+                                label="With help line and labelFor Phone"
+                                labelFor="phone"
+                                help="Help line comes here"
+                            >
+                                <Field name="phone" placeholder="Phone" id="phone" />
+                            </FormField>
 
+                            <FormField label="Dropdown Color" labelFor="color">
+                                {/* debe coincidir name con initial values de Formik */}
+                                <Field name="dropdown" id="color" component="select">
+                                    <option value="red">Red</option>
+                                    <option value="green">Green</option>
+                                    <option value="blue">Blue</option>
+                                </Field>
+                            </FormField>
+
+                            <BaseDivider />
+
+                            <FormField label="Textarea" hasTextareaHeight>
+                                <Field name="textarea" as="textarea" placeholder="Your text here" />
+                            </FormField>
+                            
+                            <BaseDivider />
 
                             <BaseButtons>
                                 <BaseButton type="submit" color="info" label="Submit"/>
