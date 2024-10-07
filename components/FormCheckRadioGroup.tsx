76e40a3e5574a5b9c4ae1f5faab5d15da9642599
erc83 <espinoza.eric@gmail.com>
2024-10-06 21:00:10 -0300

    fix: className usage FormCheckRadioGroup

--- components/FormCheckRadioGroup.tsx
+++ components/FormCheckRadioGroup.tsx
@@ -2,18 +2,19 @@ import { ReactNode, Children, cloneElement, ReactElement } from 'react'
 
 type Props = {
     isColumn?: boolean
+    className?: string
     children: ReactNode
 }
 
 const FormCheckRadioGroup = (props: Props) => {
     return (
         <div className={`flex justify-start flex-wrap -mb-3 ${props.isColumn ? 'flex-col' : ''}`}>
-            {props.children}
-            {/* {
+            
+            {
                 Children.map(props.children, (child: ReactElement) => {
-                    return cloneElement(child, {className: 'mr-6 mb-3 last:mr-0'})
+                    return cloneElement(child, {className: `mr-6 mb-3 last:mr-0 ${child.props.className}`})
                 })
-            } */}
+            }
 
         </div>
     )
