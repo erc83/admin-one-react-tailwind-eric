c27824d69ce41f1ade97aca6719e29118560168c
erc83 <espinoza.eric@gmail.com>
2024-10-09 23:31:33 -0300

    add: Carousel on dashboard Page

---
+++ src/components/Carousel/index.tsx
@@ -0,0 +1,79 @@
+import React, { useState } from 'react'
+import { mdiArrowCollapseRight, mdiArrowCollapseLeft } from '@mdi/js'
+import BaseButton from '../BaseButton'
+
+type Props = {
+    slides: string[],
+}
+
+const Carousel = ({ slides }: Props) => {
+
+    let [current, setCurrent ] = useState(0)
+
+    let previousSlice = () => {
+        if(current === 0) setCurrent(slides.length-1)
+        else setCurrent(current - 1)
+    } 
+
+    let nextSlice = () => {
+        if(current === slides.length - 1) setCurrent(0)
+        else setCurrent(current + 1)
+    } 
+
+    return (
+        <div className="overflow-hidden relative">
+            <div 
+                className={`flex transition ease-out duration-40`}
+                style={{
+                    transform: `translateX(-${current * 100}%)`
+                }}
+            >
+                {
+                    slides.map((image) => (
+                        <img src={image}/>
+                    ))
+                }
+            </div>
+            
+            <div className="absolute top-0 h-full w-full justify-between items-center flex text-white px-5 text-3xl">
+                <BaseButton 
+                    icon={mdiArrowCollapseLeft} 
+                    roundedFull
+                    onClick={previousSlice}
+                />
+                <BaseButton 
+                    icon={mdiArrowCollapseRight} 
+                    roundedFull
+                    onClick={nextSlice}
+                />
+            </div>
+            
+            <div className="absolute bottom-0 py-4 flex justify-center gap-3 w-full">
+                {
+                    slides.map((s, i) => {
+                        return (
+                            <div 
+                                key={"circle" + i}
+                                /* className="rounded-full w-5 h-5 bg-white" */
+                                className={`rounded-full w-5 h-5 cursor-pointer ${
+                                    i == current ? "bg-white" : "bg-gray-500" } 
+                                `}
+                                onClick={() => setCurrent(i )}
+                            ></div>
+                        )
+                    })
+                }
+
+            </div>
+
+
+
+        </div>
+    )
+
+
+
+}
+
+
+export default Carousel
--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -28,6 +28,7 @@ import NotificationBar from '../components/NotificationBar'
 import TableSampleClients from '../components/Table/SampleClients'
 import LayoutAuthenticated from '../layouts/Authenticated'
 import { getPageTitle } from '../config'
+import Carousel from '../components/Carousel'
 
 const Dashboard = () => {
     const { clients } = useSampleClients()
@@ -42,6 +43,13 @@ const Dashboard = () => {
 
         setChartData(sampleChartData())
     }
+
+    let slides = [
+        "https://plus.unsplash.com/premium_photo-1682091872078-46c5ed6a006d?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        "https://plus.unsplash.com/premium_photo-1661889099855-b44dc39e88c9?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        "https://plus.unsplash.com/premium_photo-1666432045848-3fdbb2c74531?q=80&w=1932&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        "https://images.unsplash.com/photo-1635792105843-143927b06230?q=80&w=2067&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+    ]
 
 
     return (
@@ -133,6 +141,12 @@ const Dashboard = () => {
 
                 
             </SectionMain>
+            
+            <div className="w-[80%] m-auto pt-6">
+                <Carousel slides={slides} />
+            </div>
+
+            
         </>
     )
 }
