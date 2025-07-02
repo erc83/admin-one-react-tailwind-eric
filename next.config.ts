a576d13e8e78cd098000d434c28f1b3e0ffe797e
erc83 <espinoza.eric@gmail.com>
2025-07-02 02:15:04 -0400

    fix: build netlify

--- next.config.ts
+++ next.config.ts
@@ -1,28 +1,27 @@
 import type { NextConfig } from "next"
 
 const nextConfig: NextConfig = {
-  /*  async redirects() {
+  basePath: "",
+/*  async redirects() {
     return [
       {
         source: "/",
         destination: 'admin-one-react-tailwind-eric',
         basePath: false,
         permanent: false
-        }
-        ]
-        },*/
-        output: 'export',
-        images: {
-          unoptimized: true,
-          /* remotePatterns: [
-            {
-              protocol: 'https',
-              hostname: 'static.justboil.me',
-            },
-          ], */
-        },
-        basePath: "/admin-one-react-tailwind-eric",
-        trailingSlash: true
+      }
+    ]
+  },*/
+  output: 'export',
+  images: {
+    unoptimized: true,
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'static.justboil.me',
+      },
+    ],
+  },
 }
 
 export default nextConfig
